31d28ef1abe6bc7700ddbbc2cc338a25f52746d0
nikhilvarshney2 <44082613+nikhilvarshney2@users.noreply.github.com>
2019-03-12 20:13:07 +0530

    prime

---
+++ prime.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, flag = 0;
+    scanf("%d", &n);
+
+    for(i = 2; i <= n/2; ++i)
+    {
+        
+        if(n%i == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (n == 1) 
+    {
+      printf("no");
+    }
+    else 
+    {
+        if (flag == 0)
+          printf("yes");
+        else
+          printf("no");
+    }
+    
+    return 0;
+}
